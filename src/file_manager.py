722416eace23cf53a1edfc99a713d63a2a580a58
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-03-07 10:32:45 -0300

    [+] broken line

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,10 +1,12 @@
 class FileManager:
     @staticmethod
-    def save_output(input_text: str, start_position: int, end_position: int):
-        with open('../text_files/output.txt', 'a', encoding='utf8') as arq_output:
-            arq_output.write(''.join(filter(lambda lista: lista[0] != '\n', input_text[start_position:end_position])))
-
-    @staticmethod
     def get_input_text():
         with open('../text_files/input.txt', encoding='utf8') as arq_input:
             return arq_input.readlines()
+
+    @staticmethod
+    def save_output(input_text: str, start_position: int, end_position: int):
+        remove_lines_unique_n = lambda lista: lista[0] != '\n'
+        with open('../text_files/output.txt', 'a', encoding='utf8') as arq_output:
+            string_escrita = ''.join(filter(remove_lines_unique_n, input_text[start_position:end_position])) + '\n'
+            arq_output.write(string_escrita)
